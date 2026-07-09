65f0fe53c09015af6645388f55ad2051ebb370a7
5XiaoWu5 <biekewulaer7@gmail.com>
2026-07-09 13:51:43 +0800

    Make auth password hashing Cloudflare compatible

--- src/features/auth/server/password.ts
+++ src/features/auth/server/password.ts
@@ -1,9 +1,32 @@
-﻿import argon2 from "argon2";
+﻿"use server";
+
+import { argon2id, argon2Verify } from "hash-wasm";
+
+const ARGON2_OPTIONS = {
+  parallelism: 1,
+  iterations: 3,
+  memorySize: 19456,
+  hashLength: 32,
+  outputType: "encoded" as const,
+};
+
+function createSalt(): Uint8Array {
+  const salt = new Uint8Array(16);
+  crypto.getRandomValues(salt);
+  return salt;
+}
 
 export async function hashPassword(password: string): Promise<string> {
-  return argon2.hash(password, { type: argon2.argon2id });
+  return argon2id({
+    password,
+    salt: createSalt(),
+    ...ARGON2_OPTIONS,
+  });
 }
 
 export async function verifyPassword(hash: string, password: string): Promise<boolean> {
-  return argon2.verify(hash, password);
+  return argon2Verify({
+    password,
+    hash,
+  });
 }
